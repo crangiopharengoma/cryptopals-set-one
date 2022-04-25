7e49a61f813b62d5040cc31f674ae1d01c2ccc1a
Sam Rosenberg <sam.rosenberg007@gmail.com>
2022-04-25 21:50:13 +1200

    Created new aes_ecb class as thin wrapper around openssl lib methods for convenience and in case I decided to change to a RustCrypto implementation later

--- src/cyphers/aes_cbc.rs
+++ src/cyphers/aes_cbc.rs
@@ -1,24 +1,20 @@
-use openssl::symm::Cipher;
-
 use crate::cyphers::aes_ecb;
 
 pub fn decrypt(encrypted_message: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
-    let cipher = Cipher::aes_128_ecb();
-
     let mut last_block = iv;
     println!("decrypting");
     encrypted_message
         .chunks(16)
         .flat_map(|chunk| {
-            let next_block = decrypt_block(key, cipher, last_block, chunk);
+            let next_block = decrypt_block(key, last_block, chunk);
             last_block = chunk;
             next_block
         })
         .collect()
 }
 
-fn decrypt_block(key: &[u8], cipher: Cipher, last_block: &[u8], chunk: &[u8]) -> Vec<u8> {
-    let decrypted_chunk = aes_ecb::decrypt_block(key, cipher, chunk);
+fn decrypt_block(key: &[u8], last_block: &[u8], chunk: &[u8]) -> Vec<u8> {
+    let decrypted_chunk = aes_ecb::decrypt_block(key, chunk);
 
     decrypted_chunk
         .iter()
--- src/cyphers/aes_ecb.rs
+++ src/cyphers/aes_ecb.rs
@@ -1,8 +1,40 @@
+use openssl::error::ErrorStack;
+use openssl::symm;
 use openssl::symm::{Cipher, Crypter, Mode};
 
 use crate::cyphers::padding;
+use crate::encoding::Digest;
 
-pub(crate) fn decrypt_block(key: &[u8], cipher: Cipher, chunk: &[u8]) -> Vec<u8> {
+/// Uses a given key to decrypt a given message digest
+///
+/// Thin wrapper around the openssl::symm::decrypt function
+pub fn try_decrypt<T: Digest>(key: &[u8], message: T) -> Result<Vec<u8>, ErrorStack> {
+    symm::decrypt(Cipher::aes_128_ecb(), key, None, message.bytes())
+}
+
+/// Uses a given key to decrypt a given message digest
+///
+/// Panics if decryption fails for any reason
+pub fn decrypt<T: Digest>(key: &[u8], message: T) -> Vec<u8> {
+    try_decrypt(key, message).expect("decryption failed")
+}
+
+/// Uses a given key to decrypt a given message digest
+///
+/// Thin wrapper around the openssl:symm:encrypt function
+pub fn try_encrypt<T: Digest>(key: &[u8], message: T) -> Result<Vec<u8>, ErrorStack> {
+    symm::encrypt(Cipher::aes_128_ecb(), key, None, message.bytes())
+}
+
+/// Uses a given key to encrypt a given message digest
+///
+/// Panics if a encryption fails for any reason
+pub fn encrypt<T: Digest>(key: &[u8], message: T) -> Vec<u8> {
+    try_encrypt(key, message).expect("decryption failed")
+}
+
+pub(crate) fn decrypt_block(key: &[u8], chunk: &[u8]) -> Vec<u8> {
+    let cipher = Cipher::aes_128_ecb();
     let mut decrypter =
         Crypter::new(cipher, Mode::Decrypt, key, None).expect("openssl failed to build Crypter");
 
